3a6da8000195b3d7955f929b8dca7304cc805461
kyoredes <kyoredes@gmail.com>
2026-05-28 19:25:07 +1000

    unused directory remove

--- src/app/infrastructure/messaging/broker.py
+++
@@ -1,7 +0,0 @@
-from faststream.rabbit import RabbitBroker
-
-from app.config import Settings
-
-
-def create_broker(settings: Settings) -> RabbitBroker:
-    return RabbitBroker(settings.rabbitmq_url)
